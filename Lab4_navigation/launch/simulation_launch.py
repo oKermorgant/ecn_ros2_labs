fd1d05822873964ff1e747080de23cc42faa0df7
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2024-02-10 13:33:48 +0100

    lab5

--- Lab4_navigation/launch/simulation_launch.py
+++ Lab4_navigation/launch/simulation_launch.py
@@ -7,7 +7,6 @@ def generate_launch_description():
     
     sl.declare_arg('map', default_value=sl.find('lab4_navigation', 'batS.yaml'))
     sl.declare_arg('map_server', default_value=True)
-
     sl.include('map_simulator', 'simulation2d_launch.py', launch_arguments= sl.arg_map('map','map_server'))
     
     # run RViz2
